c7be9b7bee50fc36502b9e28a43e8a94bccf5d44
KeithWitch <110379405+KeithWitch@users.noreply.github.com>
2022-10-15 14:53:31 +0300

    MapGoable(cpp and bp)

--- Source/TurnBased/MapGoable.cpp
+++ Source/TurnBased/MapGoable.cpp
@@ -3,30 +3,38 @@
 
 #include "MapGoable.h"
 #include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
 #include "MapGroups.h"
 
 // Sets default values
 AMapGoable::AMapGoable()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	// Creates the root component
-	RootSceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootSceneComponent"));
+	RootSceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root Scene Component"));
 	SetRootComponent(RootSceneComponent);
 
 	// Creates the box component
 	TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Trigger Box"));
 	TriggerBox->SetupAttachment(RootSceneComponent);
 	TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &AMapGoable::OnOverlapBegin);
+	TriggerBox->OnBeginCursorOver.AddDynamic(this, &AMapGoable::BeginCursorOver);
+	TriggerBox->OnEndCursorOver.AddDynamic(this, &AMapGoable::EndCursorOver);
+
+	// Creates the mesh component
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh"));
+	Mesh->SetupAttachment(RootSceneComponent);
 
 }
 
 // Called when the game starts or when spawned
 void AMapGoable::BeginPlay()
 {
+
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
@@ -37,20 +45,27 @@ void AMapGoable::Tick(float DeltaTime)
 }
 
 void AMapGoable::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-	UE_LOG(LogTemp, Warning, TEXT("heleleyle"));
-	
+{ 
+
 	AMapGroups* mapGroup = Cast<AMapGroups>(OtherActor);
-	
+
 	if (mapGroup != nullptr)
 	{
 		if (mapGroup->GetIsGoingToGoable() && mapGroup->GetNameOfGoable() == this->goableName)
 		{
-			UE_LOG(LogTemp, Warning, TEXT("ifififfif"));
 			groupBeginOverlap(mapGroup);
-
 		}
 
 	}
-	
+
+}
+
+void AMapGoable::BeginCursorOver(UPrimitiveComponent* TouchedComponent)
+{
+	Mesh->SetRenderCustomDepth(true);
+}
+
+void AMapGoable::EndCursorOver(UPrimitiveComponent* TouchedComponent)
+{
+	Mesh->SetRenderCustomDepth(false);
 }
--- Source/TurnBased/MapGoable.h
+++ Source/TurnBased/MapGoable.h
@@ -4,24 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-
 #include "MapGoable.generated.h"
 
 UCLASS()
 class TURNBASED_API AMapGoable : public AActor
 {
 	GENERATED_BODY()
-
-private:
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Meta = (AllowPrivateAccess = "true"))
-	class USceneComponent* RootSceneComponent = nullptr;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Meta = (AllowPrivateAccess = "true"))
-	class UBoxComponent* TriggerBox = nullptr;
-
-	UFUNCTION()
-	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 public:	
 	// Sets default values for this actor's properties
@@ -31,8 +19,26 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Meta = (AllowPrivateAccess = "true"))
+	class USceneComponent* RootSceneComponent = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Meta = (AllowPrivateAccess = "true"))
+	class UBoxComponent* TriggerBox = nullptr;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* Mesh = nullptr;
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	FName goableName;
+	
+	UFUNCTION()
+	virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void BeginCursorOver(UPrimitiveComponent* TouchedComponent);
+
+	UFUNCTION()
+	void EndCursorOver(UPrimitiveComponent* TouchedComponent);
 
 public:	
 	// Called every frame
